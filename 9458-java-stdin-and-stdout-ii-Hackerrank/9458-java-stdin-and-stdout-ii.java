e9f9ca06ab5f8382daafe2ae8b17555ab1e1f9a0
SHAHANA S <shahana.pandiraj@gmail.com>
2026-07-12 22:13:28 +0530

    Added solution - LeetHub

---
+++ 9458-java-stdin-and-stdout-ii-Hackerrank/9458-java-stdin-and-stdout-ii.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+   Scanner sc=new Scanner(System.in);
+   int num=sc.nextInt();
+   double dec=sc.nextDouble();
+   sc.nextLine();
+   String st=sc.nextLine();
+   System.out.println("String: " +st);
+   System.out.println("Double: " +dec);
+   System.out.println("Int: " +num);
+   sc.close();
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
