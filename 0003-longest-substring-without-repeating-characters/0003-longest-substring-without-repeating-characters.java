97884ab9a1683d9f7f84c9d5df367deea5b66e03
SHAHANA S <shahana.pandiraj@gmail.com>
2026-07-24 22:49:19 +0530

    Time: 5 ms (86.81%), Space: 46.4 MB (48.17%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -0,0 +1,33 @@
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> charSet = new HashSet<>();
+        int maxLength = 0;
+        int left = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char currentChar = s.charAt(right);
+
+            
+            while (charSet.contains(currentChar)) {
+                charSet.remove(s.charAt(left));
+                left++;
+            }
+
+        
+            charSet.add(currentChar);
+
+            
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
