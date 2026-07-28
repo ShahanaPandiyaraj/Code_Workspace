0dddc4194be6c5f6969ce3087105919631cfadbd
SHAHANA S <shahana.pandiraj@gmail.com>
2026-07-28 21:38:18 +0530

    REMOVE DUPLICATES

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+import java.util.Set;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+    Set<Integer> seen = new HashSet<>();
+    for(int num :nums)  {
+        if(seen.contains(num)){
+        return true;  
+        }
+        seen.add(num);
+    } 
+    return false;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
