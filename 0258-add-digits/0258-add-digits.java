d1ce47412503ad316463a6734c4a1da5fe9e9e8e
SHAHANA S <shahana.pandiraj@gmail.com>
2026-08-04 23:22:34 +0530

    ADD DIGITS

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int addDigits(int num) {
+    if(num==0){
+        return 0;
+    }
+    else{
+        return num%9==0?9:num%9;
+    }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
